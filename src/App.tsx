e3b5c889b95593b1b6f694a182baeb60436503de
Ariane Sousa <ariane.sousa011@gmail.com>
2021-06-23 01:05:43 -0300

    Criação do projeto e config do firebase

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "./components/Button";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Word</h1>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
